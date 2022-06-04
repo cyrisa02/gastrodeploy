1e0b4f6ab923d875e28f3318d5d673e7ad9d75d2
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-06-04 13:41:20 +0200

    Updates

---
+++ precache-manifest.734b56f4fa4a358cc25a5171e73379fa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad9e34f1aeaefd9f14d14a28539ab62",
+    "url": "/gastrodeploy/index.html"
+  },
+  {
+    "revision": "a3e68aed8ee189f3e96d",
+    "url": "/gastrodeploy/static/css/main.16eb8b80.chunk.css"
+  },
+  {
+    "revision": "9eacaa3167d104211b7d",
+    "url": "/gastrodeploy/static/js/2.a2461b66.chunk.js"
+  },
+  {
+    "revision": "eb3abb7fd2745f475c9f76f5b28765b9",
+    "url": "/gastrodeploy/static/js/2.a2461b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e68aed8ee189f3e96d",
+    "url": "/gastrodeploy/static/js/main.c809454b.chunk.js"
+  },
+  {
+    "revision": "f949997004be59aea886",
+    "url": "/gastrodeploy/static/js/runtime-main.695f275b.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/gastrodeploy/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/gastrodeploy/static/media/Questrial-Regular.37c8a1aa.ttf"
+  },
+  {
+    "revision": "a404c3657888c94e068116110f8b2c12",
+    "url": "/gastrodeploy/static/media/citron.a404c365.jpeg"
+  },
+  {
+    "revision": "cd95f39b2d8f06b70b3c622147d12226",
+    "url": "/gastrodeploy/static/media/pomme.cd95f39b.jpg"
+  },
+  {
+    "revision": "26b4742a03042ef6b7bc207838f7d1c8",
+    "url": "/gastrodeploy/static/media/tarte.26b4742a.jpg"
+  },
+  {
+    "revision": "e109e1124dc9823091ea55325ee90e41",
+    "url": "/gastrodeploy/static/media/tartec.e109e112.jpeg"
+  },
+  {
+    "revision": "cc5b8f575eee9fd428bf703ac9e77a3a",
+    "url": "/gastrodeploy/static/media/tartef.cc5b8f57.jpeg"
+  }
+]);
